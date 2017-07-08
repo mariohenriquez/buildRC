0ac8bfc0cb7e957054a8968950ddac32d9c46058
nicoleescobar <nicescobar.27@gmail.com>
2017-07-08 10:46:00 -0500

    display logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedLint = 'cssLint';
+
+  constructor(){
+    this.selectedLint = 'cssLint';
+  }
+
+  changeLint(lint:string){
+    console.log(lint)
+    this.selectedLint = lint;
+  }
+}
